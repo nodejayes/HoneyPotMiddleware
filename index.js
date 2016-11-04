7830c30a25abcbf6c9c47ef339bf44968b65265c
mgilg <markusgilg@outlook.de>
2016-11-04 18:42:56 +0100

    response send fix

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 var config  = require("./globals/config");
 var checker = require("./module/checker");
 
-function checkAddress (addr, next, req) {
+function checkAddress (addr, next, res) {
   checker(config).check(addr, function (result) {
+    console.log(result);
     if (result === null) {
       next();
     } else {
       console.log("Address was blocked: " + addr);
-      if (typeof req !== typeof undefined && req !== null) {
-        req.status(403).send("forbidden");
+      if (typeof res !== typeof undefined && res !== null) {
+        res.status(403).send();
       }
     }
   });
@@ -19,7 +20,7 @@ var HONEYPOT = function (cfg) {
 
   return {
     "checkRequest": function (req, res, next) {
-      checkAddress(req.connection.remoteAddress, next, req);
+      checkAddress(req.connection.remoteAddress, next, res);
     },
     "checkIp": function (addr, cb) {
       checkAddress(addr, cb);
