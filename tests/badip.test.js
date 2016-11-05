6c027a96ef99378924551699cba2017a8a7001fc
mgilg <markusgilg@outlook.de>
2016-11-05 09:20:43 +0100

    ipaddress test

--- tests/badip.test.js
+++ tests/badip.test.js
@@ -10,3 +10,7 @@ var mod = require("./../index")(cfg);
 mod.checkIp("192.168.33.11", function () {
   console.log("Address is good 192.168.33.11");
 });
+
+mod.checkIp("85.25.103.119", function () {
+  console.log("Address is good 85.25.103.119");
+});
